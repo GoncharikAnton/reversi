291d3edac885767f047a4caffea92e7cafb7cf43
Anton Goncharik <goncharik.a.k@gmail.com>
2022-07-24 09:03:22 +0300

    New file structure created by the project diagramm

--- model/game.py
+++ model/game.py
@@ -1,17 +1,21 @@
-from python_project_reversi.model.board import Board
-from python_project_reversi.model.players import Player
+from abc import ABC, abstractmethod
+
+from model.board import Board
+from model.players import Player
 
 
-class Game:
+class Game(ABC):
 
     def __init__(self, board_size, board: Board, plyer: Player):
         self.board_size = board_size
         self.board = board
         self.player = plyer
         self.curr_player = Player.B
+        pass
 
     def change_player(self):
         self.curr_player = 3 - self.curr_player
+        pass
 
     def is_valid_move(self, row, col):
         is_valid = False
@@ -20,9 +24,9 @@ class Game:
             for j in range(1, self.board_size + 1):
                 pass
         return False
-
-
+        pass
 
     def make_a_move(self, row, col, validation):
         if validation:
             self.board.update_cell(row, col, self.curr_player)
+        pass
