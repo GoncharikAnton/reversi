03def12723c6976a1725b9a340057acbdf691dfb
Anton Goncharik <goncharik.a.k@gmail.com>
2022-07-25 14:07:34 +0300

    I've fixed my mistakes that relied on the abstract class 'Game' in the controller.

--- controller/reversi_controller.py
+++ controller/reversi_controller.py
@@ -1,9 +1,9 @@
 from view.game_view import GameView
-from model.reversi_classic_game import ReversiClassicGame
+from model.reversi_classic_game import Game
 
 
 class GameController:
-    def __init__(self, view: GameView, model: ReversiClassicGame) -> None:
+    def __init__(self, view: GameView, model: Game) -> None:
         self.view = view
         self.model = model
 
@@ -13,10 +13,17 @@ class GameController:
             self.view.draw_board()
 
             row, col = self.view.get_move()
-            validation = self.model.is_valid_move(row, col)
-            while len(validation) == 0:
-                print('This cell is not valid, try again')
-                row, col = self.view.get_move()
-            self.model.make_a_move(row, col, validation)
-            self.model.change_player()
+            not_auto_pass = self.model.auto_pass()
+            if not_auto_pass:
+                validation = self.model.is_valid_move(row, col)
+                while len(validation) == 0:
+                    print('This cell is not valid, try again')
+                    row, col = self.view.get_move()
+                    validation = self.model.is_valid_move(row, col)
+                self.model.make_a_move(row, col, validation)
+                self.model.change_player()
+            else:
+                self.model.change_player()
+            print(self.model.check_winner())
+
 
