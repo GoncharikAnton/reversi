2432f0f38a0bbcb9b1c96b8809881586069af898
Anton Goncharik <goncharik.a.k@gmail.com>
2022-07-25 13:28:22 +0300

    Console view of the board (done).

---
+++ view/board_console_view.py
@@ -0,0 +1,30 @@
+from model.board import Board
+from view.board_view import BoardView
+
+
+class BoardConsoleView(BoardView):
+    symbols = {0: ' ', 1: 'X', 2: 'O'}
+
+    def __init__(self, board: Board):
+        super().__init__(board)
+        self.board = board
+
+    def draw_board(self):
+        board_size = self.board.board_size
+
+        for i in range(board_size):
+            if i == 0:
+                print('  ', end='')
+            print(f'{i + 1}' + ' | ', end='')
+        print('\n')
+        for i in range(board_size):
+            print('+', end='')
+            print('---+' * (board_size))
+
+            for j in range(board_size):
+                cell = self.board.get_cell(i, j)
+                print(f'| {self.symbols[cell]} ', end='')
+            print(f'| {i+1}')
+
+
+
