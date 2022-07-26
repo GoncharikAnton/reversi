56b2d3dc292b5a2a5e81a12b3a3ea994abc59332
Anton Goncharik <goncharik.a.k@gmail.com>
2022-07-26 17:28:11 +0300

    Renamed variables for clearly understanding of code. Fixed validation (validation was depended on static value).

--- model/reversi_classic_game.py
+++ model/reversi_classic_game.py
@@ -8,8 +8,7 @@ from model.directions import Directions
 
 
 class ReversiClassicGame(Game):
-    """
-    Represents classic mode of the reversi game.
+    """Represents classic mode of the reversi game.
     """
 
     def __init__(self, board_size=8):
@@ -24,18 +23,17 @@ class ReversiClassicGame(Game):
         """
         self.curr_player = 3 - self.curr_player
 
-    def is_valid_chain(self, start_position, end_position, direction, curr_player, player_2):
+    def is_valid_chain(self, start_position, end_position, direction):
         """Checks the chain that starts from start_position and goes by the direction until another players chip.
 
         :param start_position: list: [row, col]
         :param end_position: list: [row, col]
         :param direction: tuple: (x , y)
-        :param curr_player: int
-        :param player_2: int
         :return: list with valid chain of the move.
         """
+        curr_player, player_2 = self.curr_player, 3 - self.curr_player
         chain_list = [[start_position, end_position, direction]]
-        for i in range(8):
+        for i in range(self.board_size):
             if self.board.mat[end_position[0]][end_position[1]] == player_2:
                 end_position[0] += direction[0]
                 end_position[1] += direction[1]
@@ -51,24 +49,23 @@ class ReversiClassicGame(Game):
         :return: list with valid chains of moves (e.g.
         [[start_row, start_col][pont_row, point_col](direction_x, direction_y)])
         """
-        curr_player = self.curr_player
-        player_2 = 3 - self.curr_player
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        curr_position = self.board.mat[row][col]
-        flag = []
-        if curr_position != 0:
-            return flag
-        else:
-            try:
+        list_of_moves = []
+        try:
+            curr_player = self.curr_player
+            directions = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+            curr_position = self.board.mat[row][col]
+            if curr_position != 0:
+                return []
+            else:
                 for i in range(len(directions)):
-                    if col == 7 and directions[i][1] == 1:
+                    if col == self.board_size - 1 and directions[i][1] == 1:
                         direction_row, direction_col = directions[i][0], 0
-                    elif row == 7 and directions[i][1] == 1:
+                    elif row == self.board_size - 1 and directions[i][1] == 1:
                         direction_row, direction_col = 0, directions[i][1]
 
-                    elif (row == 7 and directions[i][1] == 1) and (col == 7 and directions[i][1] == 1):
+                    elif (row == self.board_size - 1 and directions[i][1] == 1) and \
+                            (col == self.board_size - 1 and directions[i][1] == 1):
                         direction_row, direction_col = 0, 0
-
                     else:
                         direction_row, direction_col = directions[i][0], directions[i][1]
 
@@ -78,60 +75,64 @@ class ReversiClassicGame(Game):
                     elif curr_position == curr_player:
                         continue
                     else:
-                        flag += self.is_valid_chain([row, col], [row + direction_row, col + direction_col],
-                                                    directions[i],
-                                                    curr_player, player_2)
-            except IndexError:
-                pass
-        return flag
+                        list_of_moves += self.is_valid_chain([row, col], [row + direction_row, col + direction_col],
+                                                             directions[i])
+        except IndexError:
+            pass
+        return list_of_moves
 
-    def make_a_move(self, row, col, validation):
+    def make_a_move(self, row, col, list_of_valid_move_chain):
         """Makes a move on the board (updates cells in accordance with current player and validation).
 
         :param row: int
         :param col: int
-        :param validation: list with valid chains of moves.
+        :param list_of_valid_move_chain: list with valid chains of moves.
         [[start_row, start_col][pont_row, point_col](direction_x, direction_y)] ...  [[][]()])
         :return: void
         """
-        for i in range(len(validation)):
-            if validation[i][2] == Directions.D:
-                for q in range(validation[i][0][0], validation[i][1][0]):
-                    self.board.update_cell(q, validation[i][1][1], self.curr_player)
+        print(list_of_valid_move_chain)
+        for i in range(len(list_of_valid_move_chain)):
+            start_cell = list_of_valid_move_chain[i][0]
+            end_cell = list_of_valid_move_chain[i][1]
+            direction = list_of_valid_move_chain[i][2]
 
-            elif validation[i][2] == Directions.U:
-                for q in range(validation[i][0][0], validation[i][1][0], -1):
-                    self.board.update_cell(q, validation[i][1][1], self.curr_player)
+            if direction == Directions.D:
+                for q in range(start_cell[0], end_cell[0]):
+                    self.board.update_cell(q, end_cell[1], self.curr_player)
 
-            elif validation[i][2] == Directions.R:
-                for q in range(validation[i][0][1], validation[i][1][1]):
-                    self.board.update_cell(validation[i][1][0], q, self.curr_player)
+            elif direction == Directions.U:
+                for q in range(start_cell[0], end_cell[0], -1):
+                    self.board.update_cell(q, end_cell[1], self.curr_player)
 
-            elif validation[i][2] == Directions.L:
-                for q in range(validation[i][0][1], validation[i][1][1], -1):
-                    self.board.update_cell(validation[i][1][0], q, self.curr_player)
+            elif direction == Directions.R:
+                for q in range(start_cell[1], end_cell[1]):
+                    self.board.update_cell(end_cell[0], q, self.curr_player)
 
-            elif validation[i][2] == Directions.UL:
-                tmp_d = validation[i][0][1]
-                for q in range(validation[i][0][0], validation[i][1][0], -1):
+            elif direction == Directions.L:
+                for q in range(start_cell[1], end_cell[1], -1):
+                    self.board.update_cell(end_cell[0], q, self.curr_player)
+
+            elif direction == Directions.UL:
+                tmp_d = start_cell[1]
+                for q in range(start_cell[0], end_cell[0], -1):
                     self.board.update_cell(q, tmp_d, self.curr_player)
                     tmp_d -= 1
 
-            elif validation[i][2] == Directions.DL:
-                tmp_d = validation[i][0][1]
-                for q in range(validation[i][0][0], validation[i][1][0] + 1):
+            elif direction == Directions.DL:
+                tmp_d = start_cell[1]
+                for q in range(start_cell[0], end_cell[0] + 1):
                     self.board.update_cell(q, tmp_d, self.curr_player)
                     tmp_d -= 1
 
-            elif validation[i][2] == Directions.DR:
-                tmp_d = validation[i][0][1]
-                for q in range(validation[i][0][0], validation[i][1][0] + 1):
+            elif direction == Directions.DR:
+                tmp_d = start_cell[1]
+                for q in range(start_cell[0], end_cell[0] + 1):
                     self.board.update_cell(q, tmp_d, self.curr_player)
                     tmp_d += 1
 
-            elif validation[i][2] == Directions.UR:
-                tmp_d = validation[i][0][1]
-                for q in range(validation[i][0][0], validation[i][1][0], -1):
+            elif direction == Directions.UR:
+                tmp_d = start_cell[1]
+                for q in range(start_cell[0], end_cell[0], -1):
                     self.board.update_cell(q, tmp_d, self.curr_player)
                     tmp_d += 1
 
